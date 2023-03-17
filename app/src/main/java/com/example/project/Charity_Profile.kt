792c4db145b693a0bbd7435c5182b1a9795ae6b9
IT21211850 <it21211850@my.sliit.lk>
2023-03-17 14:36:18 +0530

    test_one

---
+++ app/src/main/java/com/example/project/Charity_Profile.kt
@@ -0,0 +1,11 @@
+package com.example.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Charity_Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_charity_profile)
+    }
+}
---
+++ app/src/main/java/com/example/project/Charity_RegisterationForm.kt
@@ -0,0 +1,11 @@
+package com.example.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Charity_RegisterationForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_charity_registeration_form)
+    }
+}
